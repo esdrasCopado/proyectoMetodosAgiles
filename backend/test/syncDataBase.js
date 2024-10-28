65b3c3a35297d138eb05c1703ab55e5613ddba6b
Esdras Copado <copadoe6@gmail.com>
2024-10-28 14:06:10 -0700

    se cambio la configuracion de github actions

--- backend/test/syncDataBase.js
+++
@@ -1,5 +0,0 @@
-import sequelize from '../config/database.js'; // Cambia esta ruta según tu configuración
-
-before(async () => {
-  await sequelize.sync({ force: true }); // 'force: true' recreará las tablas cada vez
-});

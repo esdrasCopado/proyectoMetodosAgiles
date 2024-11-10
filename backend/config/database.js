638cfe05a3d1ce2d13d4193468c5855d091c93aa
Esdras Copado <copadoe6@gmail.com>
2024-11-09 22:32:58 -0700

    se añadio docker para mysql, node

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,8 +3,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+
 // Configura tu conexión a la base de datos
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_ROOT_PASSWORD, {
   host: process.env.DB_HOST,  // O usa '127.0.0.1'
   dialect: process.env.DB_DIALECT,
   port: process.env.DB_PORT

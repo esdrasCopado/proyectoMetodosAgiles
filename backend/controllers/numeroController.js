9c18c78e9b1339a1d4319d76939230cea1e4483d
Esdras Copado <copadoe6@gmail.com>
2024-11-27 00:19:05 -0700

    Guardar cambios locales antes de pull

--- backend/controllers/numeroController.js
+++ backend/controllers/numeroController.js
@@ -1,13 +1,12 @@
-import numeroRifa from "../models/numeroRifa.js"
-import sequelize from '../config/database.js';
+import numeroRifa from '../models/numeroRifa.js'
+import sequelize from '../config/database.js'
 
 const crearNumeroRifa = (req, res) => {
 
 }
 
 const obtenerNumerosRifa = async (req, res) => {
-
-    res.send("hola")
+  res.send('hola')
 }
 
-export default {crearNumeroRifa ,obtenerNumerosRifa}
+export default { crearNumeroRifa, obtenerNumerosRifa }
